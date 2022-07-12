f15d5909e78d20bd6e1d664759b45e2171e4b2bf
John Beech <johnbeech.github@mkv25.net>
2022-07-12 23:44:54 +0100

    Add zero data protection to Map view

--- mainui/src/views/world/Map.vue
+++ mainui/src/views/world/Map.vue
@@ -1,8 +1,8 @@
 <template>
   <multi-page-section-view class="multi-page-section world-map scroll">
     <h1><icon icon="map-marked-alt" /> World Map</h1>
-    <p>{{ saveFile.name }} Map</p>
-    <tabulation :items="[saveFile.world]" />
+    <p>Map of {{ saveFile.name }}</p>
+    <tabulation :items="[saveFile.world || {}]" />
     <world-map></world-map>
   </multi-page-section-view>
 </template>
