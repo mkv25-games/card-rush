0ca52751ba0ff010babf4e9b0e8b4719b568c131
John Beech <johnbeech.github@mkv25.net>
2023-01-01 20:51:53 +0000

    Add updated Tabulation, SmartType, RelativeDate, and HiddenByDefault components

--- mainui/src/components/ui/Tabulation.vue
+++ mainui/src/components/ui/Tabulation.vue
@@ -1,15 +1,12 @@
 <template>
-  <table
-    v-if="rows.length"
-    class="tabulation"
-  >
+  <table v-if="rows.length" class="tabulation">
     <thead>
       <tr>
         <th
           v-for="(columnHeading, i) in columnHeadings"
           :key="`colHeading-${columnHeading}-${i}`"
-          v-on:click="sortByColumnEvent(columnHeading, i)"
           :class="columnHeadingClass(columnHeading, i)"
+          @click="sortByColumnEvent(columnHeading, i)"
         >
           <div>
             <label>{{ columnHeading }}</label>
@@ -19,39 +16,14 @@
       </tr>
     </thead>
     <tbody>
-      <tr
-        v-for="(rowItem, j) in sortedRows"
-        :key="`row-${j}`"
-      >
-        <td
-          v-for="(cell, column) in rowItem"
-          :key="`cell-${column}`"
-        >
+      <tr v-for="(rowItem, j) in sortedRows" :key="`row-${j}`">
+        <td v-for="(cell, column) in rowItem" :key="`cell-${column}`">
           <slot
             :cell="cell"
             :column="column"
-            :columnKey="columnKey(column)"
+            :column-key="columnKey(column)"
           >
-            <div v-if="cellType(cell, column) === 'object'">
-              <div v-for="[entryKey, entryValue] in cellEntries(cell, column)" :key="entryKey" class="cell property">
-                <b>{{ entryKey }}</b>
-                <span>{{ entryValue }}</span>
-              </div>
-            </div>
-            <span v-else-if="cellType(cell, column) === 'icon'">
-              <icon :icon="contentsOf(cell, column)" />
-            </span>
-            <span v-else-if="cellType(cell, column) === 'image'">
-              <img :src="`http://localhost:25015/${contentsOf(cell, column)}`" /><br />
-              <label>{{ contentsOf(cell, column, false) }}</label>
-            </span>
-            <div v-else-if="cellType(cell, column) === 'color'" style="text-transform: uppercase; font-family: monospace;">
-              <span>{{ contentsOf(cell, column) }}</span>
-              <icon icon="square" :style="`color: ${contentsOf(cell, column)}`" />
-            </div>
-            <span v-else class="default">
-              {{ contentsOf(cell, column) }}
-            </span>
+            <smart-type :key="columnKey(column)" :value="contentsOf(cell, column)" />
           </slot>
         </td>
       </tr>
@@ -60,19 +32,15 @@
 </template>
 
 <script>
-import flattenObject from '../../utils/flattenObject'
+import flattenObject from '../../utils/flattenObject.js'
+import SmartType from './SmartType.vue'
 
-function removeArrayListeners (data) {
+function removeArrayListeners(data) {
   return JSON.parse(JSON.stringify(data))
 }
 
 export default {
-  data () {
-    return {
-      internalSortedColumn: '',
-      internalSortAscending: true
-    }
-  },
+  components: { SmartType },
   props: {
     items: {
       type: Array,
@@ -95,59 +63,64 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      internalSortedColumn: '',
+      internalSortAscending: true
+    }
+  },
   computed: {
-    columnHeadings () {
+    firstHeading() {
+      return this.items[0]
+    },
+    columnHeadings() {
       return this.columnKeys.map(n => n.label || n)
     },
-    columnKeys () {
+    columnKeys() {
       const columns = (this.columns && this.columns.length) ? this.columns : false
       return (columns || this.listAllProperties).map(n => n.key || n)
     },
-    groupedRows () {
+    groupedRows() {
       const index = this.items.reduce((acc, item) => {
         const lookup = item[this.row]
-        const entry = acc[lookup] || []
+        const entry = acc[lookup] ?? []
         entry.push(item)
         acc[lookup] = entry
         return acc
       }, {})
       const result = Object.entries(index).map(([key, values]) => {
-        const x = values.map(n => n)
-        return [].concat([key], ...x)
+        const nonEmpty = values.map(n => n)
+        return [].concat([key], ...nonEmpty)
       })
       return result
     },
-    listAllProperties () {
+    listAllProperties() {
       const index = this.items.reduce((acc, item) => {
-        if (typeof item === 'object') {
-          acc = Object.assign(acc, Object.keys(item))
-        } else {
-          acc = Object.assign(acc, { type: typeof item })
-        }
+        acc = Object.assign(acc, Object.keys(item))
         return acc
       }, {})
       return Object.values(index)
     },
-    plainRows () {
+    plainRows() {
       return this.items.map(item => {
         return this.columnKeys.map(() => item)
       })
     },
-    row () {
+    row() {
       return (this.columnKeys)[0]
     },
-    rows () {
+    rows() {
       return (this.groupItems) ? this.groupedRows : this.plainRows
     },
-    computedSortedColumn () {
-      return this.sortedColumn || this.internalSortedColumn
+    computedSortedColumn() {
+      return this.sortedColumn ?? this.internalSortedColumn
     },
-    computedSortAscending () {
+    computedSortAscending() {
       return this.sortedColumn ? this.sortAscending : this.internalSortAscending
     },
-    sortedRows () {
+    sortedRows() {
       const { rows, sortedColumn, internalSortedColumn, internalSortAscending } = this
-      const dataPreSorted = !!sortedColumn
+      const dataPreSorted = sortedColumn ? true : false
       if (dataPreSorted) {
         return rows
       }
@@ -159,8 +132,8 @@ export default {
       const clonedRows = removeArrayListeners(rows)
       if (col) {
         clonedRows.sort((a, b) => {
-          const sa = JSON.stringify(a[0][col] || a[0]) || ''
-          const sb = JSON.stringify(b[0][col] || b[0]) || ''
+          const sa = JSON.stringify(a[0][col])
+          const sb = JSON.stringify(b[0][col])
           return asc ? sa.localeCompare(sb) : sb.localeCompare(sa)
         })
       }
@@ -168,41 +141,26 @@ export default {
     }
   },
   methods: {
-    cellEntries (cell, column) {
+    cellEntries(cell, column) {
       const value = this.contentsOf(cell, column)
       const flattened = flattenObject(value)
       return Object.entries(flattened)
     },
-    cellType (cell, column) {
-      const value = this.contentsOf(cell, column)
-      const columnKey = this.columnKey(column)
-      if (columnKey === 'icon') {
-        return 'icon'
-      }
-      if (columnKey === 'color') {
-        return 'color'
-      }
-      if (columnKey === 'image') {
-        return 'image'
-      }
-      return typeof value
-    },
-    columnHeadingClass (columnHeading, i) {
+    columnHeadingClass(columnHeading) {
       return columnHeading === this.computedSortedColumn ? 'sorted' : 'unsorted'
     },
-    columnIcon (columnHeading, i) {
+    columnIcon(columnHeading) {
       const direction = this.computedSortAscending ? 'sort-amount-up' : 'sort-amount-down'
       return this.computedSortedColumn === columnHeading ? direction : 'sort'
     },
-    contentsOf (cell, column, showCellIfEmpty = true) {
-      const columnKey = this.columnKeys[column] || column
-      const columnContents = cell[columnKey]
-      return columnContents === undefined && showCellIfEmpty ? cell : columnContents
+    contentsOf(cell, column) {
+      const columnKey = this.columnKeys[column] ?? column
+      return cell[columnKey] ?? ''
     },
-    columnKey (column) {
-      return this.columnKeys[column] || column
+    columnKey(column) {
+      return this.columnKeys[column] ?? column
     },
-    sortByColumnEvent (columnHeading) {
+    sortByColumnEvent(columnHeading) {
       const same = columnHeading === this.computedSortedColumn
       this.internalSortedColumn = columnHeading
       this.internalSortAscending = same ? !this.internalSortAscending : true
